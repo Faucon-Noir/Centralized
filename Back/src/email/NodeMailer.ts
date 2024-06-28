a11ca4a44518d0596690ea5d6cb804cdae794837
Mathieu <beuret_m@etna-alternance.net>
2024-06-28 10:12:33 +0200

    Edit .env

--- Back/src/email/NodeMailer.ts
+++ Back/src/email/NodeMailer.ts
@@ -3,27 +3,31 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export class NodeMailerSendEmail {
-    public mailApp: string = "centralized2024@gmail.com";
+	public mailApp: string = process.env.MAIL_APP;
 
-    public transporter = nodemailer.createTransport({
-        host: "mail.openjavascript.info",
-        port: 465,
-        secure: true,
-        auth: {
-            user: this.mailApp,
-            pass: process.env.MAIL_KEY
-        },
-        tls: {
-            rejectUnauthorized: false // ne pas vérifier le certificat du serveur SMTP
-        }
-    });
+	public transporter = nodemailer.createTransport({
+		host: process.env.MAIL_HOST,
+		port: 465,
+		secure: true,
+		auth: {
+			user: this.mailApp,
+			pass: process.env.MAIL_KEY,
+		},
+		tls: {
+			rejectUnauthorized: false, // ne pas vérifier le certificat du serveur SMTP
+		},
+	});
 
-    public async sendMailTicket(email: string, subject: string, username: string) {
-        const mailOptions = {
-            from: this.mailApp,
-            to: email,
-            subject: subject,
-            html: `<html>
+	public async sendMailTicket(
+		email: string,
+		subject: string,
+		username: string
+	) {
+		const mailOptions = {
+			from: this.mailApp,
+			to: email,
+			subject: subject,
+			html: `<html>
                     <head>
                         <style>
                         </style>
@@ -42,15 +46,15 @@ export class NodeMailerSendEmail {
                         <p>L'équipe Centralized</p>
                     </body>
                     </html>`,
-        };
+		};
 
-        try {
-            const info = await this.transporter.sendMail(mailOptions);
-            console.log("Message sent: %s", info.messageId);
-            return { success: info.messageId };
-        } catch (error) {
-            console.error(error);
-            return { error: error.message };
-        }
-    }
+		try {
+			const info = await this.transporter.sendMail(mailOptions);
+			console.log("Message sent: %s", info.messageId);
+			return { success: info.messageId };
+		} catch (error) {
+			console.error(error);
+			return { error: error.message };
+		}
+	}
 }
