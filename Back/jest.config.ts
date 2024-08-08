9d67e99448f5ad19f715a3a07915607369873d5c
Mathieu <beuret_m@etna-alternance.net>
2024-08-08 11:37:51 +0200

    Code Coverage back

--- Back/jest.config.ts
+++ Back/jest.config.ts
@@ -1,10 +1,20 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  moduleNameMapper: {
-    // Other mappers if needed
-  },
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
-  },
-  testEnvironment: 'node',
+	moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+	moduleNameMapper: {
+		// Other mappers if needed
+	},
+	transform: {
+		"^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+	},
+	testEnvironment: "node",
+	// COVERAGE
+	collectCoverage: true,
+	coverageDirectory: "./coverage",
+	coverageReporters: ["lcov", "text"],
+	collectCoverageFrom: [
+		"src/**/*.{ts,tsx}",
+		"!src/**/*.d.ts",
+		"!src/**/*.index.ts",
+		"!src/tests/**",
+	],
 };
