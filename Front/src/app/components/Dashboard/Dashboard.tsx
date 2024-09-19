39be6a5869aa28ac63224496e9671b7313a9f4bb
Yousef <abdelo_y@etna-alternance.net>
2024-09-19 20:13:41 +0200

    cancel ok

--- Front/src/app/components/Dashboard/Dashboard.tsx
+++ Front/src/app/components/Dashboard/Dashboard.tsx
@@ -64,7 +64,6 @@ export default function Dashboard({ page = '', userData, updateUserData }: { pag
 					delete updatedSelctedMap[id]; // Supprimer l'entrée correspondant à `id`
 					return updatedSelctedMap; // Retourner l'objet mis à jour
 				});
-				console.log("ok")
 			}
 		}
 	}
