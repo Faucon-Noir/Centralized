50be9fa1da3d7c5f8321186d2bf8c71487fe25ab
fernan_d <fernan_d@etna-alternance.net>
2024-10-24 17:16:22 +0200

    Finish swiper test

--- Front/src/app/components/Swiper/customSwiper/indexCustomSwiper.cy.tsx
+++ Front/src/app/components/Swiper/customSwiper/indexCustomSwiper.cy.tsx
@@ -8,6 +8,7 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 describe('CustomSwiper', () => {
   it('renders', () => {
+    cy.viewport(1920, 1080);
     // see: https://on.cypress.io/mounting-react
     cy.mount(<div>
       <div className='Entete'>
@@ -31,6 +32,8 @@ describe('CustomSwiper', () => {
                 updateUserData={{}}
                 userData={{}}
             />
+        </SwiperSlide>
+        <SwiperSlide key={"vn,;"}>
             <ProjetCard
                 name={"test"}
                 totalTickets={42}
@@ -40,6 +43,8 @@ describe('CustomSwiper', () => {
                 updateUserData={{}}
                 userData={{}}
             />
+        </SwiperSlide>
+        <SwiperSlide key={"vn,;"}>
             <ProjetCard
                 name={"test"}
                 totalTickets={42}
@@ -49,6 +54,8 @@ describe('CustomSwiper', () => {
                 updateUserData={{}}
                 userData={{}}
             />
+        </SwiperSlide>
+        <SwiperSlide key={"vn,;"}>
             <ProjetCard
                 name={"test"}
                 totalTickets={42}
@@ -58,6 +65,8 @@ describe('CustomSwiper', () => {
                 updateUserData={{}}
                 userData={{}}
             />
+        </SwiperSlide>
+        <SwiperSlide key={"vn,;"}>
             <ProjetCard
                 name={"test"}
                 totalTickets={42}
@@ -67,15 +76,8 @@ describe('CustomSwiper', () => {
                 updateUserData={{}}
                 userData={{}}
             />
-            <ProjetCard
-                name={"test"}
-                totalTickets={42}
-                key={"ghjkl"}
-                id={1}
-                projectId={1}
-                updateUserData={{}}
-                userData={{}}
-            />
+        </SwiperSlide>
+        <SwiperSlide key={"vn,;"}>
             <ProjetCard
                 name={"test"}
                 totalTickets={42}
@@ -90,5 +92,99 @@ describe('CustomSwiper', () => {
   </CustomSwiper>
     </div>
     )
+  })
+
+  it('Verifier fleches', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<div>
+      <div className='Entete'>
+        <div className='TitleProjetCards'>
+          <h2>Mes projets</h2>
+       </div>
+        <div className='fleches'>
+            <ChevronLeftIcon className='swiper-button-prev swiper-button-prev-1 fleche' />
+            <ChevronRightIcon className='swiper-button-next swiper-button-next-1 fleche' />
+        </div>
+    </div>
+      <CustomSwiper swiperId={1}>
+      <div className='ProjetCards'>
+        <SwiperSlide key={"vn,;"}>
+            <ProjetCard
+                name={"1er"}
+                totalTickets={42}
+                key={"ghjkl"}
+                id={1}
+                projectId={1}
+                updateUserData={{}}
+                userData={{}}
+            />
+        </SwiperSlide>
+        <SwiperSlide key={"vn,;"}>
+            <ProjetCard
+                name={"2eme"}
+                totalTickets={42}
+                key={"ghjkl"}
+                id={1}
+                projectId={1}
+                updateUserData={{}}
+                userData={{}}
+            />
+        </SwiperSlide>
+      </div>
+  </CustomSwiper>
+    </div>
+    )
+    cy.get('.swiper-button-prev-1').should('have.css', 'color').and('eq', 'rgb(128, 128, 128)');
+    cy.get('.swiper-button-next-1').click();
+    cy.get('.swiper-button-next-1').should('have.css', 'color').and('eq', 'rgb(128, 128, 128)');
+    cy.get('.swiper-button-prev-1').click();
+    cy.get('.swiper-button-prev-1').should('have.css', 'color').and('eq', 'rgb(128, 128, 128)');
+  })
+
+  it('Verifier Card', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<div>
+      <div className='Entete'>
+        <div className='TitleProjetCards'>
+          <h2>Mes projets</h2>
+       </div>
+        <div className='fleches'>
+            <ChevronLeftIcon className='swiper-button-prev swiper-button-prev-1 fleche' />
+            <ChevronRightIcon className='swiper-button-next swiper-button-next-1 fleche' />
+        </div>
+    </div>
+      <CustomSwiper swiperId={1}>
+      <div className='ProjetCards'>
+        <SwiperSlide key={"1"}>
+            <ProjetCard
+                name={"1er"}
+                totalTickets={43}
+                key={"1"}
+                id={1}
+                projectId={1}
+                updateUserData={{}}
+                userData={{}}
+            />
+        </SwiperSlide>
+        <SwiperSlide key={"2"}>
+            <ProjetCard
+                name={"2eme"}
+                totalTickets={42}
+                key={"2"}
+                id={1}
+                projectId={1}
+                updateUserData={{}}
+                userData={{}}
+            />
+        </SwiperSlide>
+      </div>
+  </CustomSwiper>
+    </div>
+    )
+    cy.get('.swiper-slide-active').should('contain.text', '1er');
+    cy.get('.swiper-button-next-1').click();
+    cy.get('.swiper-slide-active').should('contain.text', '2eme');
+    cy.get('.swiper-button-prev-1').click();
+    cy.get('.swiper-slide-active').should('contain.text', '1er');
   })
 })
