6736710e26ec4b437f940ed0c22f1e9bc6d87fed
Athéna <ytblackfalcon@gmail.com>
2024-11-08 04:34:36 +0100

    fix url

--- Front/src/pages/_app.tsx
+++ Front/src/pages/_app.tsx
@@ -59,7 +59,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	}, [router.events]);
 
 	useEffect(() => {
-		if (Component.name != 'LoginPage' && Component.name != 'WelcomePage') {
+		if (Component.name != 'b' && Component.name != 'h') {
 			UserData().then((result) => {
 				setUserData(result);
 				setLoading(false); // Une fois que les données sont prêtes, on arrête le chargement
@@ -89,13 +89,12 @@ export default function App({ Component, pageProps }: AppProps) {
 	console.log(Component.name);
 	return (
 		<main className={myFont.className} style={{ height: '100%' }}>
-			{Component.name == 'LoginPage' ||
-			Component.name == 'WelcomePage' ? (
+			{Component.name == 'b' || Component.name == 'h' ? (
 				<Component {...pageProps} />
 			) : (
 				<AuthWrapper>
 					<TaskProvider>
-						{Component.name === 'LoginPage' ? (
+						{Component.name === 'b' ? (
 							<Component {...pageProps} userData={userData} />
 						) : windowWidth >= 1280 ? (
 							<Grid container style={{ height: '100%' }}>
