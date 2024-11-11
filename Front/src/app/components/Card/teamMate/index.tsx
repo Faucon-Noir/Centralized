c60a69f6c7e38efdc50336cce5bcb1c47e1987e6
dacruz_j <dacruz_j@etna-alternance.net>
2024-11-11 03:54:55 +0100

    on regle des card sur le dashboard

--- Front/src/app/components/Card/teamMate/index.tsx
+++ Front/src/app/components/Card/teamMate/index.tsx
@@ -11,18 +11,24 @@ export default function TeamMateCard({
 	bio,
 }: TeamMateCardProps) {
 	return (
-		<div className='memberTeamCard'>
-			<Avatar
-				id='avatar'
-				src={'/media/'+avatar}
-				sx={{ height: '100px', width: '100px' }}
-			/>
-			<Typography
-				id='name'
-				style={{ fontSize: '18px', fontWeight: '700' }}
-			>
-				{`${firstName} ${lastName}`}
-			</Typography>
-		</div>
+		<div className='card_user'>
+				<div className='card-header yellow'>
+					<img
+						src={'/media/' + avatar}
+						alt='avatar'
+						className='avatar'
+						onError={(e) => {
+							e.currentTarget.src = '/assets/avatar_team.png';
+						}}
+					></img>
+				</div>
+				<div className='card-body'>
+					<h2 className='user-name'>{`${firstName} ${lastName}`}</h2>
+					<div className='card-footer'>
+						<div className='user-info'>
+						</div>
+					</div>
+				</div>
+			</div>
 	);
 }
