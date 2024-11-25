caed7688f25a77590fc8bafd617fb94c1cec6902
Yousef <abdelo_y@etna-alternance.net>
2024-11-25 20:16:50 +0100

    ok

--- Front/.next/server/server-reference-manifest.js
+++ Front/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"GTwkg7RKQOaBPYG4c+v76y94S2hz0Xc6VeqKadfH6Rc=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"HKhhnChQokuCgaPz8rytQm8jaRAquA78wN3gMVYwzEk=\"}"
