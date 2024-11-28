8408f46cd0e9f4ce8315e12539056092b4071380
Yousef <abdelo_y@etna-alternance.net>
2024-11-28 23:41:21 +0100

    ok

--- Back/src/services/stripeService.ts
+++ Back/src/services/stripeService.ts
@@ -7,74 +7,74 @@ dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-10-28.acacia', });
 
 
-interface UserStripe { 
-    id: string; 
-    mail: string; 
-    lastname: string; 
-    firstname: string; 
+interface UserStripe {
+    id: string;
+    mail: string;
+    lastname: string;
+    firstname: string;
 
-} 
+}
 
-const createStripeCustomer = async (user: UserStripe) => { 
-    try { 
-        const customer = await stripe.customers.create({ 
-            email: user.mail, 
-            name: user.lastname + ' ' + user.firstname, 
-            metadata: { userId: user.id, }, 
-        }); 
-        return customer; 
-    } catch (error) { 
-        console.error('Error creating customer: ', error); 
-        throw error; 
-    } 
+const createStripeCustomer = async (user: UserStripe) => {
+    try {
+        const customer = await stripe.customers.create({
+            email: user.mail,
+            name: user.lastname + ' ' + user.firstname,
+            metadata: { userId: user.id, },
+        });
+        return customer;
+    } catch (error) {
+        console.error('Error creating customer: ', error);
+        throw error;
+    }
 };
 
 const paymentStripe = async (price: string, customer_id: string, data: User) => {
     try {
-        const session = stripe.checkout.sessions.create({ 
-            payment_method_types: ['card'], 
-            line_items: [ 
-                { 
-                    price: price, 
-                    quantity: 1, 
-                }, 
-            ], 
-            mode: 'subscription', 
-            success_url: 'http://localhost:3000/login?status_stripe=success', 
-            cancel_url: 'http://localhost:3000/login?status_stripe=error', 
-            customer: customer_id, 
+        const session = stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price: price,
+                    quantity: 1,
+                },
+            ],
+            mode: 'subscription',
+            success_url: 'https://centralized.fr/login?status_stripe=success',
+            cancel_url: 'https://centralized.fr/login?status_stripe=error',
+            customer: customer_id,
         })
         return session;
     } catch (error) {
-        console.error('Error creating customer: ', error); 
-        throw error; 
+        console.error('Error creating customer: ', error);
+        throw error;
     }
 }
 
 const getProductStripe = async (productId: string) => {
     try {
-        const prices =stripe.prices.list({ 
-            product: productId, 
+        const prices = stripe.prices.list({
+            product: productId,
             limit: 1, // Ajuste cette valeur selon tes besoins 
         });
         return prices;
     } catch (error) {
-        console.error('Error creating customer: ', error); 
-        throw error; 
+        console.error('Error creating customer: ', error);
+        throw error;
     }
 }
 
-async function findCustomerByEmail(email: string) { 
-    try { 
-        const customers = await stripe.customers.search({ 
-            query : 'email:"'+email+'"',
+async function findCustomerByEmail(email: string) {
+    try {
+        const customers = await stripe.customers.search({
+            query: 'email:"' + email + '"',
             limit: 1
-        }); 
-        return customers.data; 
-    } catch (err) { 
-        console.error(`Error searching for customer: ${err.message}`); 
-        return null; 
-    } 
+        });
+        return customers.data;
+    } catch (err) {
+        console.error(`Error searching for customer: ${err.message}`);
+        return null;
+    }
 }
 
 export { createStripeCustomer, UserStripe, paymentStripe, getProductStripe, findCustomerByEmail };
--- Front/src/app/components/Polling/PollingStripe/index.tsx
+++ Front/src/app/components/Polling/PollingStripe/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { useTask } from '../../../contexts/isReq'; // Assurez-vous que c'est le bon chemin
 import './style.scss';
 import React from 'react';
-export default function PollingStripe({status}: {status:string}) {
+export default function PollingStripe({ status }: { status: string }) {
 
 	const { isTaskComplete, completeTask } = useTask();
 	const [showPopup, setShowPopup] = useState(false);
@@ -12,19 +12,19 @@ export default function PollingStripe({status}: {status:string}) {
 	useEffect(() => {
 		let intervalId: any;
 
-			if (!isTaskComplete) {
-				setShowPopup(true); // Affiche la popup quand le polling commence
+		if (!isTaskComplete) {
+			setShowPopup(true); // Affiche la popup quand le polling commence
 
-				const checkTaskStatus = () => {
-					intervalId = setInterval(async () => {
-						setShowPopup(false); // Fermer la popup
-					    clearInterval(intervalId); // Arrêter le polling
-					}, 20000); // Polling toutes les 20 secondes
-				};
+			const checkTaskStatus = () => {
+				intervalId = setInterval(async () => {
+					setShowPopup(false); // Fermer la popup
+					clearInterval(intervalId); // Arrêter le polling
+				}, 20000); // Polling toutes les 20 secondes
+			};
 
-				checkTaskStatus();
-			}
-			return () => clearInterval(intervalId); // Cleanup l'interval si le composant se démonte
+			checkTaskStatus();
+		}
+		return () => clearInterval(intervalId); // Cleanup l'interval si le composant se démonte
 	}, [
 		isTaskComplete,
 		completeTask,
@@ -34,13 +34,13 @@ export default function PollingStripe({status}: {status:string}) {
 		<>
 			{showPopup && (status === "success") && (
 				<div className='req colorSuccess'>
-					<p>Paiement de l'abonnement effectué avec succès.</p>
+					<p>Le paiement de l&apos;abonnement à été effectué avec succès.</p>
 					<p>Vous pouvez dès maintenant vous connecter pour découvrir Centralized.</p>
 				</div>
 			)}
 			{showPopup && (status === "error") && (
 				<div className='req colorError'>
-					<p>Le paiement de l'abonnement n'a pas abouti.</p>
+					<p>Le paiement de l&apos;abonnement n&apos;a pas abouti.</p>
 					<p>Veuillez vous connecter afin de relancer le processus de paiement.</p>
 				</div>
 			)}
