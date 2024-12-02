d9019229e373167351192c45f460db42799ff36e
dacruz_j <dacruz_j@etna-alternance.net>
2024-12-02 17:01:35 +0100

    on change le css des formulaires de creation de cahier des charges

--- Front/src/app/components/Form/secondStep/index.tsx
+++ Front/src/app/components/Form/secondStep/index.tsx
@@ -49,176 +49,179 @@ function SecondStep({
 		<div className='step2form'>
 			<div className='wrapperform'>
 				<h1>Crée un nouveau projet</h1>
-				<div className='line'>
-					<input
-						type='text'
-						placeholder='Titre du projet'
-						onChange={(e) =>
-							setProject({
-								...project,
-								name: e.target.value.trim(),
-							})
+				<div className='w-45'>
+					<div className='small'>
+						<input
+							type='text'
+							placeholder='Titre du projet'
+							onChange={(e) =>
+								setProject({
+									...project,
+									name: e.target.value.trim(),
+								})
+							}
+							required
+						/>
+						<input
+							type='text'
+							placeholder='Description du projet'
+							onChange={(e) =>
+								setProject({
+									...project,
+									description: e.target.value.trim(),
+								})
+							}
+						/>
+					</div>
+					<div>
+						<textarea
+							placeholder='Fonctionnalités du projet'
+							rows={5}
+							cols={50}
+							onChange={(e) =>
+								setProject({
+									...project,
+									functionality: e.target.value.trim(),
+								})
+							}
+						/>
+						<textarea
+							placeholder='Technologies du projet'
+							rows={5}
+							cols={30}
+							onChange={(e) =>
+								setProject({
+									...project,
+									technology: e.target.value.trim(),
+								})
+							}
+						/>
+					</div>
+					<div>
+						<textarea
+							placeholder="Planning prévisionnel&#10;&#x0A;
+							Remplir le formulaire: 1 heure, Repartir les tickets: 2 jours; Finir le projet: 3 mois"
+							rows={5}
+							cols={50}
+							onChange={(e) =>
+								setProject({
+									...project,
+									forecast: e.target.value.trim(),
+								})
+							}
+						/>
+						<textarea
+							placeholder="Répartition de l'équipe"
+							rows={5}
+							cols={100}
+							onChange={(e) =>
+								setProject({
+									...project,
+									teamRole: e.target.value.trim(),
+								})
+							}
+						/>
+					</div>
+					<div className='small'>
+						<input
+							type='date'
+							placeholder='Date de début'
+							onChange={(e) =>
+								setProject({
+									...project,
+									start_date: e.target.value.trim(),
+								})
+							}
+						/>
+						<input
+							type='date'
+							placeholder='Date de fin'
+							onChange={(e) =>
+								setProject({
+									...project,
+									end_date: e.target.value.trim(),
+								})
+							}
+						/>
+					</div>
+					<div className='centered'>
+						<input
+							type='text'
+							placeholder='Budget prévisionnel'
+							onChange={(e) =>
+								setProject({
+									...project,
+									budget: e.target.value.trim(),
+								})
+							}
+						/>
+						<select
+							onChange={(e) =>
+								setProject({
+									...project,
+									team: e.target.value.trim(),
+								})
+							}
+							required
+						>
+							<option value=''>Veuillez choisir une équipe</option>
+							{userData?.team
+								? userData?.team.map((item: any) => (
+									<option
+										key={item.team.id}
+										value={item.team.id}
+									>
+										{item.team.name}
+									</option>
+								))
+								: null}
+						</select>
+						
+					</div>
+					<div>
+						<textarea
+							placeholder='Contraintes du projet'
+							rows={5}
+							cols={30}
+							onChange={(e) =>
+								setProject({
+									...project,
+									constraints: e.target.value.trim(),
+								})
+							}
+						/>
+						<textarea
+							placeholder='Conditions de validation'
+							rows={5}
+							cols={30}
+							onChange={(e) =>
+								setProject({
+									...project,
+									validation: e.target.value.trim(),
+								})
+							}
+						/>
+					</div>
+					<button
+						onClick={handleSubmit}
+						disabled={
+							!project.name ||
+							!project.description ||
+							!project.functionality ||
+							!project.forecast ||
+							!project.start_date ||
+							!project.end_date ||
+							!project.budget ||
+							!project.technology ||
+							!project.constraints ||
+							!project.validation ||
+							!project.team ||
+							!project.teamRole
 						}
-						required
-					/>
-					<input
-						type='text'
-						placeholder='Description du projet'
-						onChange={(e) =>
-							setProject({
-								...project,
-								description: e.target.value.trim(),
-							})
-						}
-					/>
-				</div>
-				<div className='line'>
-					<textarea
-						placeholder='Fonctionnalités du projet'
-						rows={5}
-						cols={50}
-						onChange={(e) =>
-							setProject({
-								...project,
-								functionality: e.target.value.trim(),
-							})
-						}
-					/>
-					<textarea
-						placeholder="Planning prévisionnel&#10;&#x0A;
-						Remplir le formulaire: 1 heure, Repartir les tickets: 2 jours; Finir le projet: 3 mois"
-						rows={5}
-						cols={50}
-						onChange={(e) =>
-							setProject({
-								...project,
-								forecast: e.target.value.trim(),
-							})
-						}
-					/>
-				</div>
-				<div className='line'>
-					<input
-						type='date'
-						placeholder='Date de début'
-						onChange={(e) =>
-							setProject({
-								...project,
-								start_date: e.target.value.trim(),
-							})
-						}
-					/>
-					<input
-						type='date'
-						placeholder='Date de fin'
-						onChange={(e) =>
-							setProject({
-								...project,
-								end_date: e.target.value.trim(),
-							})
-						}
-					/>
-					<input
-						type='text'
-						placeholder='Budget prévisionnel'
-						onChange={(e) =>
-							setProject({
-								...project,
-								budget: e.target.value.trim(),
-							})
-						}
-					/>
-				</div>
-				<div className='line'>
-					<select
-						onChange={(e) =>
-							setProject({
-								...project,
-								team: e.target.value.trim(),
-							})
-						}
-						required
 					>
-						<option value=''>Veuillez choisir une équipe</option>
-						{userData?.team
-							? userData?.team.map((item: any) => (
-								<option
-									key={item.team.id}
-									value={item.team.id}
-								>
-									{item.team.name}
-								</option>
-							))
-							: null}
-					</select>
+						Générer
+					</button>
 				</div>
-				<div className='line'>
-					<textarea
-						placeholder='Technologies du projet'
-						rows={5}
-						cols={30}
-						onChange={(e) =>
-							setProject({
-								...project,
-								technology: e.target.value.trim(),
-							})
-						}
-					/>
-					<textarea
-						placeholder='Contraintes du projet'
-						rows={5}
-						cols={30}
-						onChange={(e) =>
-							setProject({
-								...project,
-								constraints: e.target.value.trim(),
-							})
-						}
-					/>
-					<textarea
-						placeholder='Conditions de validation'
-						rows={5}
-						cols={30}
-						onChange={(e) =>
-							setProject({
-								...project,
-								validation: e.target.value.trim(),
-							})
-						}
-					/>
-				</div>
-				<div className='line'>
-					<textarea
-						placeholder="Répartition de l'équipe"
-						rows={5}
-						cols={100}
-						onChange={(e) =>
-							setProject({
-								...project,
-								teamRole: e.target.value.trim(),
-							})
-						}
-					/>
-				</div>
-				<button
-					onClick={handleSubmit}
-					disabled={
-						!project.name ||
-						!project.description ||
-						!project.functionality ||
-						!project.forecast ||
-						!project.start_date ||
-						!project.end_date ||
-						!project.budget ||
-						!project.technology ||
-						!project.constraints ||
-						!project.validation ||
-						!project.team ||
-						!project.teamRole
-					}
-				>
-					Générer
-				</button>
 			</div>
 		</div>
 	);
